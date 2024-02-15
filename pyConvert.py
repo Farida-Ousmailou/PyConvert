98f797a95ba81e22ec74fc25e7a8723c1d8c8019
Farida Ousmailou <faridaousmailou51@gmail.com>
2024-02-16 00:33:43 +0100

    add security measures

--- pyConvert.py
+++ pyConvert.py
@@ -7,14 +7,17 @@ def converter(option, value):
         - value (float): The value to be converted.
          Prints the converted value with appropriate unit.
         """
+    validate_value(value)
+    if option not in ["cm vers pouce", "pouce vers cm"]:
+        raise ValueError("Option invalide...")
+
     if option == "cm vers pouce":
         result = cm_to_inch(value)
         print("Le resultat est " + str(result) + " pouce")
     elif option == "pouce vers cm":
         result = inch_to_cm(value)
         print("Le resultat est " + str(result) + " cm")
-    else:
-        print("Option invalide. Veuillez saisir 'cm vers pouce' ou 'pouce vers cm'. ")
+
 
 def cm_to_inch(value):
     """
@@ -24,6 +27,7 @@ def cm_to_inch(value):
     Returns:
     float: The converted value in inches.
     """
+    validate_value(value)
     return value * 0.394
 
 def inch_to_cm(value):
@@ -34,28 +38,42 @@ def inch_to_cm(value):
     Returns:
     float: The converted value in centimeters.
     """
+    validate_value(value)
     return value * 2.54
 
+def validate_value(value):
+    """
+    Validate the input value for conversion.
+
+    Parameters:
+    - value (float): The value to be validated.
+
+    Returns:
+    None: Raises TypeError if value is not numeric or ValueError if value is negative.
+    """
+    if not isinstance(value, (int, float)):
+        raise TypeError("La valeur doit être de type numérique (int ou float)")
+    if value < 0:
+        raise ValueError("La valeur ne peut pas être négative")
 
 
 if __name__ == "__main__":
-
+    value_to_convert = None
     option = input("souhaitez vous convertir pouce vers cm ou cm vers  pouce?")
+    while option not in ["cm vers pouce", "pouce vers cm"]:
+        option = input("Veuillez saisir la bonne conversion entre:  pouce vers cm ou cm vers  pouce")
 
-    if option == " ":
-        option = input("veillez saisir la bonne conversion")
-    elif option == int:
-        option = input("veillez saisir la bonne conversion")
+    while not isinstance(value_to_convert, (int, float)):
+        try:
+            value_to_convert = float(input("Saisissez le nombre à convertir: "))
+        except ValueError:
+            print("La valeur saisie n'est pas valide. Veuillez saisir un nombre.")
 
-    value_to_convert = float(input("saisissez le nombre à convertir:"))
+    print("Vous souhaitez convertir de " + option)
 
-    if value_to_convert == " ":
-        value_to_convert = input("veillez saisir un nombre")
-    elif value_to_convert == str:
-        value_to_convert = input("veillez saisir un nombre")
-    else:
-        print("vous souhaitez convertir" + option)
-
-    converter(option, value_to_convert)
+    try:
+        converter(option, value_to_convert)
+    except Exception as e:
+        print(e)
 
 
