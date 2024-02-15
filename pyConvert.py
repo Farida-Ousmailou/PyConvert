1d7c60116870d32c7c60acb8c3dd58231b036e03
Farida Ousmailou <faridaousmailou51@gmail.com>
2024-02-15 23:52:08 +0100

    add docstring

--- pyConvert.py
+++ pyConvert.py
@@ -1,7 +1,12 @@
 
 def converter(option, value):
-    """pouce = 2.54 cm
-    cm = 0.394 pouce"""
+    """
+        Convert between centimeters and inches based on the specified option.
+        Parameters:
+        - option (str): The conversion option. Should be either "cm vers pouce" or "pouce vers cm".
+        - value (float): The value to be converted.
+         Prints the converted value with appropriate unit.
+        """
 
     if option == (" cm vers pouce") :
         result = cm_to_inch(value)
@@ -11,12 +16,28 @@ def converter(option, value):
         print("le resultat est " + str(result) + " cm")
 
 def cm_to_inch(value):
+    """
+    Convert centimeters to inches.
+    Parameters:
+    - value (float): The value in centimeters to be converted to inches.
+    Returns:
+    float: The converted value in inches.
+    """
     return value * 0.394
+
 def inch_to_cm(value):
+    """
+    Convert inches to centimeters.
+    Parameters:
+    - value (float): The value in inches to be converted to centimeters.
+    Returns:
+    float: The converted value in centimeters.
+    """
     return value * 2.54
 
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
 
     option = input("souhaitez vous convertir pouce vers cm ou cm vers  pouce?")
 
