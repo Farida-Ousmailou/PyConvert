ff6ddd03cc95ce186d7c49a540d5f19b3f31bc1b
Farida Ousmailou <faridaousmailou51@gmail.com>
2024-02-15 23:46:29 +0100

    refactoring

--- pyConvert.py
+++ pyConvert.py
@@ -1,32 +1,39 @@
 
-def converter (option, value_to_convert):
+def converter(option, value):
     """pouce = 2.54 cm
     cm = 0.394 pouce"""
 
     if option == (" cm vers pouce") :
-        result = value_to_convert * 0.394
+        result = cm_to_inch(value)
         print("le resultat est " + str(result) + " pouce")
     elif option == ("pouce vers cm") :
-        result = value_to_convert * 2.54
+        result = inch_to_cm(value)
         print("le resultat est " + str(result) + " cm")
-    return result
+
+def cm_to_inch(value):
+    return value * 0.394
+def inch_to_cm(value):
+    return value * 2.54
 
 
-option = input("souhaitez vous convertir pouce vers cm ou cm vers  pouce?")
-if option == " ":
-    option = input("veillez saisir la bonne conversion")
-elif option == int:
-    option = input("veillez saisir la bonne conversion")
+if __name__ == "__main__" :
 
-value_to_convert = float (input("saisissez le nombre à convertir:"))
-if value_to_convert == " ":
-    value_to_convert = input("veillez saisir un nombre")
-elif value_to_convert == str:
-    value_to_convert = input("veillez saisir un nombre")
-else:
-    print("vous souhaitez convertir" + option)
+    option = input("souhaitez vous convertir pouce vers cm ou cm vers  pouce?")
 
+    if option == " ":
+        option = input("veillez saisir la bonne conversion")
+    elif option == int:
+        option = input("veillez saisir la bonne conversion")
 
-result = converter(option, value_to_convert)
+    value_to_convert = float (input("saisissez le nombre à convertir:"))
+
+    if value_to_convert == " ":
+        value_to_convert = input("veillez saisir un nombre")
+    elif value_to_convert == str:
+        value_to_convert = input("veillez saisir un nombre")
+    else:
+        print("vous souhaitez convertir" + option)
+
+    converter(option, value_to_convert)
 
 
